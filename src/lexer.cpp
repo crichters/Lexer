8ee663b873fbc0549f6ce59d8af3770ff5a72fcb
gray <gray@lilith.lilith>
2019-08-30 23:04:50 -0400

    Filling in skeleton for producing queue of tokens from input string

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,9 +1,69 @@
 #include "include/lexer.h"
 
+// Begin LEXER _ PUBLIC
+
 Lexer::Lexer(){
-	std::cout << "Lexer initialized" << std::endl;
 }
 
 Lexer::~Lexer(){
-	std::cout << "Lexer deconstructed" << std::endl;
 }
+
+std::queue<std::string> Lexer::getTokenFromString(std::string input){
+	return this->parseString(input);
+}
+
+std::queue<std::string> Lexer::getTokenFromFile(std::string path){
+
+	if(FILE *file = fopen(path.c_str(), "r")){
+		fclose(file);
+	} else {
+		throwError("Error, cannot open file \"" + path + "\"");
+	}
+
+	std::ifstream fileBuffer(path);
+	std::string fileText((std::istreambuf_iterator<char>(fileBuffer)), std::istreambuf_iterator<char>());
+
+	return this->parseString(fileText);
+}
+
+uint Lexer::testPairs(int x, int y){
+	return hashPairs(x, y);
+}
+
+
+// End LEXER _ Public
+
+
+// Begin LEXER _ Private
+
+std::queue<std::string> Lexer::parseString(std::string input){
+
+	std::queue<std::string> tokens;
+	std::string token;
+
+	for(char c : input){
+
+		if(c == ' '){
+			tokens.push(token);
+			token = "";
+		} else {
+			token += c;
+		}
+	}
+
+	tokens.push(token);
+
+	return tokens;
+}
+
+void Lexer::throwError(std::string errorMessage){
+	errorMessage += '\n';
+	fprintf(stderr, errorMessage.c_str());
+	exit(1);
+}
+
+uint Lexer::hashPairs(int x, int y){
+	return pow(2, 16) * x + y;
+}
+
+// End LEXER _ Private
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "include/lexer.h"
 
+#include <queue>
+
 using namespace std;
 
 int main(){
 
 	Lexer lx;
+
+	queue<string> tokens = lx.getTokenFromFile("test/hello_world.lx");
+
+	int x = tokens.size();
+	
+	for(int i = 0; i < x; i++){
+		cout << tokens.front() << endl;
+		tokens.pop();
+	}
+
 
 	return 0;
 }
